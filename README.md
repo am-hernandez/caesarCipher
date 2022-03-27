9f9a3c6c9254b1ed1257b7c5b524da31f5f0a7c8
am-hernandez <angel@mouseandweb.com>
2022-03-27 01:16:10 -0400

    update: modify functions to shift in opposite direction

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Caesar Cypher
+# Caesar Cipher
 
 Encrypts a secret message using a simple shift mechanism. Enter a shift number to set how many characters each character is shifted.
 
-
 ## Usage
 
-Cypher: Enter a secret message in the secret text box. Then choose a shift number. Press submit to generate the encrypted message.\
-Decypher: Enter the encoded message and enter the secret key to decipher the message. Press submit to generate the secret message.\
+Cipher: Enter a secret message in the secret text box. Then choose a shift number. Press submit to generate the encrypted message.\
+Decipher: Enter the encoded message and enter the secret key to decipher the message. Press submit to generate the secret message.\
 \
-Try it [here](https://am-hernandez.github.io/caesarCypher/) now in github.
+Try it [here](https://am-hernandez.github.io/caesarCipher/) now in github.
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)
--- js/caesar.js
+++ js/caesar.js
@@ -1,68 +1,64 @@
 // submit buttons
-let cypherSubmit = document.getElementById("submit_cypher");
-let decypherSubmit = document.getElementById("submit_decypher");
+let cipherSubmit = document.getElementById("submit_cipher");
+let decipherSubmit = document.getElementById("submit_decipher");
 // get the h3 elements that will display encrypted and decrypted messages
 let encryptedP = document.getElementById("encrypted");
 let decryptedP = document.getElementById("decrypted");
 
-
 // encoding the secret message
-cypherSubmit.addEventListener("click", function(){
-    let secretVal = document.getElementById("secret").value;
-    let shiftVal = Number(document.getElementById("shift").value);
-    let message1 = caesarCypher(secretVal, shiftVal);
-    encryptedP.innerText = message1;
+cipherSubmit.addEventListener("click", function () {
+  let secretVal = document.getElementById("secret").value;
+  let shiftVal = Number(document.getElementById("shift").value);
+  let message1 = caesarCipher(secretVal, shiftVal);
+  encryptedP.innerText = message1;
 });
 
 // decoding the secret message
-decypherSubmit.addEventListener("click", function(){
-    let cypherVal = document.getElementById("cypher").value;
-    let keyVal = Number(document.getElementById("key").value);
-    let message2 = caesarDecypher(cypherVal, keyVal);
-    decryptedP.innerText = message2;
+decipherSubmit.addEventListener("click", function () {
+  let cipherVal = document.getElementById("cipher").value;
+  let keyVal = Number(document.getElementById("key").value);
+  let message2 = caesarDecipher(cipherVal, keyVal);
+  decryptedP.innerText = message2;
 });
 
 // functions below
-// cypher
-function caesarCypher(secret, shift) {
-    let alpha = "abcdefghijklmnopqrstuvwxyz";
-    let cypher = "";
-    secret = secret.toLowerCase();
-    for (let i = 0; i < secret.length; i++){
-        let char = secret[i];
-        if (alpha.includes(char)) {
-            let pos = alpha.indexOf(char);
-            let newPos = pos+shift;
-            if (newPos >= 26) {
-                newPos = newPos % 26;
-            }
-            let newChar = alpha[newPos];
-            cypher += newChar;
-        } else {
-            cypher += char;
-        }
+// cipher
+function caesarCipher(message, key) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let secret = "";
+  message = message.toUpperCase();
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+    let pos = alphabet.indexOf(char);
+    if (pos > -1) {
+      let newPos = pos - key;
+      if (newPos < 0) {
+        newPos = 25 + ((newPos + 1) % 26);
+      }
+      let newChar = alphabet[newPos];
+      secret += newChar;
+    } else {
+      secret += char;
     }
-    return cypher;
+  }
+  return secret;
 }
 
-//decypher
-function caesarDecypher(cypher, key){
-    let alpha = "abcdefghijklmnopqrstuvwxyz";
-    let decypher = "";
-    cypher = cypher.toLowerCase();
-    for (let i = 0; i < cypher.length; i++){
-        let char = cypher[i];
-        if (alpha.includes(char.toLowerCase())) {
-            let pos = alpha.indexOf(char);
-            let newPos = pos-key;
-            if (newPos < 0) {
-                newPos = 25-((newPos+1)*-1)%26;
-            }
-            let newChar = alpha[newPos];
-            decypher += newChar;
-        } else {
-            decypher += char;
-        }
+//decipher
+function caesarDecipher(secret, shift) {
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let message = "";
+  secret = secret.toUpperCase();
+  for (let i = 0; i < secret.length; i++) {
+    let char = secret[i];
+    let pos = alphabet.indexOf(char);
+    if (pos > -1) {
+      let newPos = (pos + shift) % 26;
+      let newChar = alphabet[newPos];
+      message += newChar;
+    } else {
+      message += char;
     }
-    return decypher;
+  }
+  return message;
 }
